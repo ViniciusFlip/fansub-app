304ac8732563d035e3093b92d5c32cbfbacf97b0
ViniciusFlip <viniciusaraujoflip@gmail.com>
2026-06-22 12:41:21 -0300

    Add files via upload

---
+++ admin/hub/js/guard.js
@@ -0,0 +1,31 @@
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js"
+
+
+import {
+    auth
+} from "../../firebase/config.js"
+
+
+
+onAuthStateChanged(auth, (user)=>{
+
+
+    if(!user){
+
+        window.location.href =
+        "/auth/index.html"
+
+        return
+
+    }
+
+
+    console.log(
+        "Usuário logado:",
+        user.email
+    )
+
+
+})
